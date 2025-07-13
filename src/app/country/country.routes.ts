710baa28ad6b1ebc4ef0fc727cbc87ec5aafa7ed
Ericka Ponce <erickaponce92@gmail.com>
2025-07-13 16:04:58 -0500

    feat: create base directory structure

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { ContryPageComponent } from "./pages/country-page/country-page.component";
+
+export const contryRoutes: Routes = [
+  {
+    path: '',
+    component: ContryPageComponent
+  },
+]
