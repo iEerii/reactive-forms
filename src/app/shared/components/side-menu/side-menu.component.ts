d37f90462df16325a4f3644ec61292d90f5044a7
Ericka Ponce <erickaponce92@gmail.com>
2025-07-13 15:18:27 -0500

    feat: create new project

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  imports: [],
+  template: `<p>side-menu works!</p>`,
+  styleUrl: './side-menu.component.css',
+})
+export class SideMenuComponent { }
